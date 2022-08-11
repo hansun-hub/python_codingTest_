f6063038157ec7d8b4f55768e49ef189d834609d
hansun-hub <hansun33@naver.com>
2022-08-12 07:34:34 +0900

    2022_08_12_씨름선수(그리디)

---
+++ AA/섹션4_이분탐색_그리디/씨름선수(그리디).py
@@ -0,0 +1,14 @@
+import sys
+n=int(input())
+body = []
+for i in range(n):
+    a,b = map(int, input().split())
+    body.append((a,b))
+body.sort(reverse=true)
+largest=0
+for x,y in body:
+    if y>largest:
+        largest=y
+        cnt+=1
+print(cnt)
+
