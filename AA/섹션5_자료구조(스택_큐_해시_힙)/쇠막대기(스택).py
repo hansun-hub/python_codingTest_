4fc4f2d66cc53b0882aabd4c0ffd8ee7de8e9139
hansun-hub <hansun33@naver.com>
2022-08-18 10:58:45 +0900

    2022-08-18_쇠막대기(스택)

---
+++ AA/섹션5_자료구조(스택_큐_해시_힙)/쇠막대기(스택).py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin=open("input.txt","rt")
+s=input()
+stack=[]
+for i in range(len(s)):
+    if s[i]=='(':
+        stack.append(s[i])
+    else:
+        stack.pop()
+        if s[i-1]=='(':
+            cnt+=len(stack)
+        else:
+            cnt+=1
+print(cnt)
