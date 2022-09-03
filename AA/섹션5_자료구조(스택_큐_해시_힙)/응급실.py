a1be7c20607e68312c9345d6e64262707847181d
hansun-hub <hansun33@naver.com>
2022-09-03 10:19:08 +0900

    2022-09-03_아나그램(딕셔너리)

--- AA/섹션5_자료구조(스택_큐_해시_힙)/응급실.py
+++ AA/섹션5_자료구조(스택_큐_해시_힙)/응급실.py
@@ -1,3 +1,6 @@
+import sys
+from collections import deque
+sys.stdin = open("input.txt","r")
 n,m = map(int,input().split())
 Q = [(pos,val) for pos,val in enumerate(list(map(int,input().split())))]
 Q = deque(Q)
