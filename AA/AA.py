08f2a0a061f11a2993128a86e10f207994438284
hansun-hub <hansun33@naver.com>
2022-09-04 08:06:51 +0900

    2022-09-04_아나그램(리스트해쉬)

--- AA/AA.py
+++ AA/AA.py
@@ -1,2 +1,13 @@
-import sys
-sys.stdin=open("input.txt","rt")
+h,m = map(int,input().split())
+
+if m<44:
+    tmp=45-m
+    m=60-tmp
+    if(h==0):
+        h=23
+    else:
+        h=h-1
+else:
+    m=-45
+
+print(h,m)
---
+++ AA/섹션5_자료구조(스택_큐_해시_힙)/아나그램(리스트해쉬).py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open("input.txt","r")
+
+a=input()
+b=input()
+
+str1=[0]*52
+str2=[0]*52
+ 
+
+for x in a:
+    if x.isupper():
+        str1[org(x)-65]+=1
+    else:
+        str1[org(x)-71]+=1
+
+for x in b:
+    if x.isupper():
+        str2[org(x)-65]+=1
+    else:
+        str2[org(x)-71]+=1
+
+for i in range(52):
+    if str1[i] != str2[i]:
+        print("NO")
+        break
+else:
+    print("YES")
